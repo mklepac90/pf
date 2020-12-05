1f08b4512ca3d0bb415f05353b79390fcb5f366d
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-12-05 16:00:32 -0500

    Add styling for chapters

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-import { Box, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import fs from "fs";
 import matter from "gray-matter";
 import Link from "next/link";
@@ -10,28 +18,80 @@ import Logo from "../public/logo-black.svg";
 export default function Index({ posts }) {
   return (
     <Layout>
-      <Text as="h1" mb={8} fontSize="2xl">
-        Immigrant Handbook
-      </Text>
-      <Box as="p" mb={8} textAlign="center">
-        We made the mistakes, so you don't have to
-      </Box>
-      <List>
-        {posts.map((post) => (
-          <ListItem key={post.filePath} mb={2} fontSize="3xl" fontWeight="bold">
-            <Link
-              as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`}
-              href={`/posts/[slug]`}
+      <Grid gridTemplateRows="auto 1fr auto" height="100vh">
+        <Box background="black">
+          <Flex flexDirection="column" justify="center" align="center">
+            <Box mt="4rem">
               <Logo />
-            >
-              <a>{post.data.title}</a>
-            </Link>
-            <Text fontSize="md" fontWeight="normal">
-              {post.data.description}
+            </Box>
+            <Text mb={8} color="white" textAlign="center" maxW={["40%", "30%"]}>
+              We made the mistakes, so you don't have to We made the mistakes,
+              so you don't have to We made the mistakes, so you don't have to We
+              made the mistakes, so you don't have to We made the mistakes, so
+              you don't have to
             </Text>
-          </ListItem>
-        ))}
-      </List>
+          </Flex>
+        </Box>
+        <Box background="white">
+          <Grid
+            gridTemplateColumns="repeat(auto-fit, minmax(240px, 400px)) minmax(240px, 400px)"
+            gridRowGap="2rem"
+            gridColumnGap="2rem"
+            gridAutoColumns="1fr"
+            gridAutoFlow="row dense"
+            gridTemplateRows="auto"
+            maxW="1100px"
+            mx="auto"
+            px="8rem"
+            mt="4rem"
+          >
+            <Text
+              as="h2"
+              gridColumnStart="span 2"
+              gridColumnEnd="span 2"
+              fontSize="2em"
+              color="#ec7f37"
+            >
+              Chapters
+            </Text>
+            <Box>
+              <Text as="h3" fontSize="1.5em" color="#ec7f37">
+                Chapter 1: On Arrival
+              </Text>
+              <Text as="h5" fontSize="0.7em" color="hsl(225deg, 15%, 50%)">
+                Getting all the ducks in order
+              </Text>
+            </Box>
+            <Box>
+              <Text as="h3" fontSize="1.5em" color="#ec7f37">
+                Chapter 2: Transport
+              </Text>
+              <Text as="h5" fontSize="0.7em" color="hsl(225deg, 15%, 50%)">
+                Getting around
+              </Text>
+            </Box>
+            <Box>
+              <Text as="h3" fontSize="1.5em" color="#ec7f37">
+                Chapter 3: Govt
+              </Text>
+              <Text as="h5" fontSize="0.7em" color="hsl(225deg, 15%, 50%)">
+                Getting important documents
+              </Text>
+            </Box>
+            <Box>
+              <Text as="h3" fontSize="1.5em" color="#ec7f37">
+                Chapter 4: Preethika
+              </Text>
+              <Text as="h5" fontSize="0.7em" color="hsl(225deg, 15%, 50%)">
+                Call me Preethika
+              </Text>
+            </Box>
+          </Grid>
+        </Box>
+        <Box fontSize="0.7em" textAlign="center">
+          Made in Toronto
+        </Box>
+      </Grid>
     </Layout>
   );
 }
