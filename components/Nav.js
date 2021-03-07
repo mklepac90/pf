b795739bef4d680818d22ed6b8e0f68d37202e43
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2021-03-07 11:15:08 -0500

    Add dark mode

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,11 @@
-import { Flex, Text } from '@chakra-ui/layout';
 import React from 'react';
 import Link from 'next/link';
+import { FaSun, FaMoon } from 'react-icons/fa';
+
+import { Flex, Icon, Text, useColorMode } from '@chakra-ui/react';
 
 const Nav = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex as="nav" flexDir="column" pr={8}>
       <Text fontWeight="bold" pb={2}>
@@ -17,6 +20,11 @@ const Nav = () => {
       <Link href="/reading">
         <a>reading</a>
       </Link>
+      {colorMode === 'dark' ? (
+        <Icon my={2} onClick={toggleColorMode} as={FaSun} />
+      ) : (
+        <Icon my={1} onClick={toggleColorMode} as={FaMoon} />
+      )}
     </Flex>
   );
 };
--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -4,6 +4,7 @@ import path from 'path';
 // POSTS_PATH is useful when you want to get the path to a specific file
 export const POSTS_PATH = path.join(process.cwd(), 'posts');
 export const NEWSLETTER_PATH = path.join(process.cwd(), 'newsletter');
+export const READING_PATH = path.join(process.cwd(), 'reading');
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
@@ -13,5 +14,10 @@ export const postFilePaths = fs
 
 export const newsletterFilePaths = fs
   .readdirSync(NEWSLETTER_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path));
+
+export const readingFilePaths = fs
+  .readdirSync(READING_PATH)
   // Only include md(x) files
   .filter((path) => /\.mdx?$/.test(path));
