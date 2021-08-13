3431c18bd6bbe5bb30a5cd3c16142c73b02420cb
Michael Klepac <67074804+mklepac90@users.noreply.github.com>
2021-08-12 20:46:31 -0400

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,23 +1,7 @@
-const title = 'Yesh Yendamuri';
-const description = 'personal website of yesh yendamuri';
+const title = 'Michael Klepac';
+const description = 'personal website of michael klepac';
 const SEO = {
   title,
   description,
-  canonical: 'https://yeshyendamuri.vercel.app/',
-  openGraph: {
-    type: 'website',
-    locale: 'en_IE',
-    url: 'https://yeshyendamuri.vercel.app/',
-    title,
-    description,
-    images: [
-      {
-        url: 'https://yeshyendamuri.vercel.app/me.jpg',
-        alt: title,
-        width: 1280,
-        height: 720,
-      },
-    ],
-  },
 };
 export default SEO;
