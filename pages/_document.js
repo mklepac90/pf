d77bb0b9af3d37feab14466054cc8aa91d7e1db1
Michael Klepac <67074804+mklepac90@users.noreply.github.com>
2021-08-12 20:57:50 -0400

    comment favicons

--- pages/_document.js
+++ pages/_document.js
@@ -14,23 +14,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
             rel="stylesheet"
           ></link>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="https://yeshyendamuri.vercel.app/assets/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="https://yeshyendamuri.vercel.app/assets/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="https://yeshyendamuri.vercel.app/assets/favicon-16x16.png"
-          />
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="" /> */}
+          {/* <link rel="icon" type="image/png" sizes="32x32" href="" /> */}
+          {/* <link rel="icon" type="image/png" sizes="16x16" href="" /> */}
         </Head>
         <body>
           <Main />
